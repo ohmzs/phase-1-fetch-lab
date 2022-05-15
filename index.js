ab646843f448f6280d3992e1ea3a2e30432e8898
ohmzs <38644134+ohmzs@users.noreply.github.com>
2022-05-15 14:21:47 -0400

    commit tree a34e42cc82c1eaebce14bd892370d342d15b2707

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+// access the fetch() request you will create inside fetchBooks(). //write your solution so that fetchBooks() returns the fetch(). 
+// document.addEventListener('DOMContentLoaded', function fetchBooks () {
+//   
+//    .then(books => {
+//           fetchBooks(books);
+//    });
+//    });
+
+
+
+
+   function fetchBooks(){
+    return fetch('https://anapioficeandfire.com/api/books')
+        .then(response => response.json())
+        .then(books => {
+          renderBooks(books)
+          console.log(books)
+        })
+   }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
+   
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+};
+document.addEventListener('DOMContentLoaded', function(){
   fetchBooks();
 });
